7e84762c162d0c5864df7bdbfc464cdbab0e4271
Manohar Kurapati <kvmanohar@gmail.com>
2018-04-02 18:50:53 +0100

    Save message data to database

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -46,16 +46,26 @@ class ChatViewController: UIViewController {
     
     //MARK: - Send & Recieve from Firebase
     
-    
-    
-    
-    
     @IBAction func sendPressed(_ sender: AnyObject) {
         
+        messageTextfield.endEditing(true)
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
         
-        //TODO: Send the message to Firebase and save it in our database
-        
-        
+        let messageDB = Database.database().reference().child("Messages")
+        let messageDictionary = ["Sender":Auth.auth().currentUser?.email,
+                                 "MessageBody": messageTextfield.text!]
+        messageDB.childByAutoId().setValue(messageDictionary){
+            (error, reference) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Message saved successfully!")
+                self.messageTextfield.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextfield.text = ""
+            }
+        }
     }
     
     //TODO: Create the retrieveMessages method here:
